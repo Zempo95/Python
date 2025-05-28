3a769130e1a711b2ccb3b246dada440db52a8d91
Daniel Zempoalteca <danielzempolteca@gmail.com>
2025-05-27 23:21:31 -0600

    Finished

--- Python/Day1/Proyecto.py
+++ Python/Day1/Proyecto.py
@@ -1,14 +1,15 @@
 
 Dolares = float(input( "Inregresa tus dolares a convertir: "))
-conversion = Dolares * 6
+Conversion = Dolares * 6
 
 Billetes100 = 100
 Billetes20 = 20
 
-Recibir100 = conversion // Billetes100
-Recibir20 = conversion % Billetes100
+Recibir100 = Conversion // Billetes100
+Recibir20 = Conversion % Billetes100
 Recibir20 = Recibir20 // Billetes20
-
-print(f"Recibiras {Recibir100} de 100 y {Recibir20} billetes de 20")
+Pesos = Conversion - (Recibir100 * 100  + Recibir20 *20)  
+print(f" recibiras en Euros la cantidad de {Conversion}")
+print(f"Recibiras {Recibir100} de 100 y {Recibir20} billetes de 20 y {Pesos} en pesos")
 
 
