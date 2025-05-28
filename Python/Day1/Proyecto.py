12edb8d2c4966e9580d31d2c87ddbac3cb97e0e0
Daniel Zempoalteca <danielzempolteca@gmail.com>
2025-05-27 22:57:00 -0600

    Agregar convertidor4

--- Python/Day1/Proyecto.py
+++ Python/Day1/Proyecto.py
@@ -1,12 +1,21 @@
 nombre = str(input("Inregresa tu nombre: "))
 fecha = str(input("Ingresa la fecha: "))
 saludo = "Buenos dias"
+billetecien = 100
+billetesveinte = 20
 
-print(f"{saludo} , {nombre}")
 
+print(f"{saludo}  {nombre}")
 dolares = float(input("Ingresa aquí los dolares: "))
-dolarestoeuros = dolares * 0.88
+dolarestoeuros = dolares * 4
 print(f"tu cambio de divisa es por: {dolarestoeuros} ")
+
+billeteCien = dolarestoeuros / billetecien
+billetesVeinte= dolares % billetesveinte
+
+print(f"USted recibira {billeteCien} de cien y {billetesVeinte} billestes de veinte")
+
+
 
 
 
