14e03f83fbd1b74562de5b163e91e7aa78f0cfac
Daniel Zempoalteca <danielzempolteca@gmail.com>
2025-05-28 19:38:33 -0600

    Metodos

---
+++ Python/Day2/Metodos.py
@@ -0,0 +1,13 @@
+#Upper  para mayusculas
+#help(manzana.capitalize
+palabra = "miperrito"
+
+palabra_may = palabra.capitalize()
+print(palabra_may)
+print(dir(palabra_may))
+
+# ejemplo_dir.py
+
+
+
+
