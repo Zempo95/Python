1e947e914576202061e90274d1c96074b1fd9048
Daniel Zempoalteca <danielzempolteca@gmail.com>
2025-05-29 19:14:48 -0600

    mas ejercicios

---
+++ Python/Day2/Slincing.py
@@ -0,0 +1,21 @@
+#Dada la palabra "murciélago", imprime la letra en la posición 6.
+#Extrae los primeros 4 caracteres de "murciélago".
+#Extrae la última letra usando índice negativo.
+#Imprime la palabra "murciélago" al revés.
+#Extrae los caracteres del índice 2 al 6 (sin incluir el 6).
+# m-0
+# u-1
+# r-2
+# c-3
+# i-4
+# e-5
+# l-6
+# a-7
+# g-8
+# o-9
+palabra = "murcielago"
+print(palabra[6])
+print(palabra[0:4])
+print(palabra[-1])
+print(palabra[2:6])
+
