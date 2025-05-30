8ddbed7b19d5d9724a6f8a73140b8984df651a9e
Daniel Zempoalteca <danielzempolteca@gmail.com>
2025-05-29 19:30:04 -0600

    mas ejercicios

--- Python/Day2/Slincing.py
+++ Python/Day2/Slincing.py
@@ -13,9 +13,24 @@
 # a-7
 # g-8
 # o-9
+"""
 palabra = "murcielago"
 print(palabra[6])
 print(palabra[0:4])
 print(palabra[-1])
+print(palabra[::-1])
 print(palabra[2:6])
+"""
 
+#Extrae los primeros 6 caracteres de "murciélago" saltando de 2 en 2.
+#Extrae todos los caracteres en posiciones pares (índices 0, 2, 4...) de "murciélago".
+#Extrae todos los caracteres en posiciones impares.
+#Imprime la palabra desde el índice 7 hasta el 2 en orden inverso.
+#¿Qué imprime "murciélago"[::3]?
+
+palabra = "murcielago"
+print(palabra[0:6:2])
+print(palabra[0::2])
+print(palabra[0::0+1])
+print(palabra[-7:-2])
+#mclo
